933bb1044ba4526ca925011ddf83af80a551f9a3
Jaiyant Rawat <jaiyantrawat99@gmail.com>
2021-01-09 18:41:55 +0530

    09-01-21

---
+++ preprocessor.c
@@ -0,0 +1,18 @@
+/* Preprocessor Code: Area of circle */
+
+#include<stdio.h>
+#define PI 3.14
+
+void main()
+{
+	float radius, perimeter, area,PI = 3.14;
+	
+	printf("Enter the radius of circle = ");
+	scanf("%f",radius);
+	perimeter = 2*PI*radius;
+	area = PI*radius*radius;
+	printf("Radius of circle = %0.2f \n Perimeter of circle = %0.2f \nArea of circle = %0.2f");
+	
+}
+
+
